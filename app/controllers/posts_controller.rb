5cc2cce1ede0b461908a9db8ed19157384c80ab5
alvin-odins <alvin.odins@gmail.com>
2016-06-07 13:23:39 -0400

    Add association between poet and user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,19 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@posts = Post.all.order('created_at DESC')
+		@posts = Post.all.order("created_at DESC")
+	end
+
+	def show
 	end
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
+
 		if @post.save
 			redirect_to @post
 		else
@@ -19,17 +23,14 @@ class PostsController < ApplicationController
 		end
 	end
 
-	def show
-	end
-
 	def edit
 	end
 
 	def update
 		if @post.update(post_params)
-			redirect_to@post
+			redirect_to @post
 		else
-			redner 'edit'
+			render 'edit'
 		end
 	end
 
